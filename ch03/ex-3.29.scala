a7e9f4493209bcbc812be59e7416f48228096147
Leandro <leandro.ferro@gmail.com>
2018-10-31 21:57:07 -0400

    Last one for ch 03.

--- ch03/ex-3.29.scala
+++ ch03/ex-3.29.scala
@@ -1,1 +1,18 @@
-def fold[A, B](tree: Tree[A])(z: A => B, c: (A, A) => B)
+def fold[A, B](tree: Tree[A])(z: A => B, c: (B, B) => B): B = 
+  tree match {
+    case Leaf(v) => z(v)
+    case Branch(l, r) => c(fold(l)(z, c), fold(r)(z, c))
+  }
+
+def map[A, B](tree: Tree[A])(f: A => B): Tree[B] =
+  fold[A, Tree[B]](tree)(v => Leaf(f(v)), (l, r) => Branch(l, r))
+
+def depth[A](tree: Tree[A]): Int = 
+  fold[A, Int](tree)(_ => 1, (l, r) => 1 + (l max r))
+
+def maximum(tree: Tree[Int]): Int =
+  fold[Int, Int](tree)(identity, (l, r) => l max r)
+
+def size[A](tree: Tree[A]): Int =
+  fold[A, Int](tree)(_ => 1, (l, r) => 1 + l + r)
+
